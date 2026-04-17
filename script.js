32d8079eb60e5d4d2e07e008f149df520c5e13e6
Kyi_Ricky <cristalkyi095@gmail.com>
2026-04-18 02:45:35 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const lyrics = [
   "Dear Lord, when I get to heaven",
-  "Please Let Me Bring My MAN",
+  "Please let me bring my MAN",
   "When he comes, tell me that you'll let him in",
   "Father tell me if you can",
+  "All that grace",
+  "All that body",
+  "All that face",
+  "Makes me want to party",
+  "He is my SUN"
+  "He makes me shine",
+  "like Diamonds",
 ];
 
 let index = 0;
@@ -10,21 +17,12 @@ let index = 0;
 const container = document.getElementById("lyrics");
 
 function waveText(text){
-  const container = document.getElementById("lyrics");
-  container.innerHTML = "";
-  container.classList.add("wave");
-
-  text.split("").forEach((char, i)=> {
-    const span = document.createElement("span");
-    span.innerText = char;
-    span.style.animationDelay = i*0.05 + "s";
-    container.appendChild(span);
-  });
+  container.innerText = text;
 }
 
 function fadeOut(callback){
   container.style.transition = "opacity 1s ease";
-  container.style.opacity= 0;
+  container.style.opacity = 0;
 
   setTimeout(() => {
     callback();
@@ -34,16 +32,36 @@ function fadeOut(callback){
 
 function showLyrics(){
   if(index < lyrics.length){
-    waveText(lyrics[index]);
+    fadeOut(() => {
+      waveText(lyrics[index]);
+    });
+
     index++;
-    setTimeout(showLyrics, 4000);
+    setTimeout(showLyrics, 4500);
+  } else {
+    showEnding();
   }
+}
+
+function showEnding(){
+  fadeOut(() => {
+    container.innerText = "hope u r entertained";
+
+    container.style.fontSize = "clamp(1.2rem, 4vw, 2rem)";
+    container.style.letterSpacing = "3px";
+
+    container.style.textShadow =
+      "0 0 8px rgba(255,255,255,0.6), " +
+      "0 0 20px rgba(230,230,250,0.4), " +
+      "0 0 30px rgba(78,167,233,0.3)";
+  });
 }
 
 document.getElementById("startBtn").onclick = () => {
   document.getElementById("overlay").style.display = "none";
+
   const iframe = document.getElementById("music");
   iframe.style.display = "block";
 
-  showLyrics();
+  setTimeout(showLyrics, 1000);
 };
