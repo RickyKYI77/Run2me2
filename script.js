12c8e525a87f7f82a33455bdf5bcf865dac0c289
Kyi_Ricky <cristalkyi095@gmail.com>
2026-04-18 02:14:01 +0630

    Fix syntax errors

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
-const lyrics - [
-  "Dear Lord, when I get to heaven"
-  "Please Let Me Bring My MAN"
-  "When he comes, tell me that you'll let him in"
-  "Father tell me if you can"
+const lyrics = [
+  "Dear Lord, when I get to heaven",
+  "Please Let Me Bring My MAN",
+  "When he comes, tell me that you'll let him in",
+  "Father tell me if you can",
 ];
 
 let index = 0;
+
+const container = document.getElementById("lyrics");
 
 function waveText(text){
   const container = document.getElementById("lyrics");
   container.innerHTML = "";
   container.classList.add("wave");
 
-  text.split("").forEach((char, i))=> {
+  text.split("").forEach((char, i)=> {
     const span = document.createElement("span");
     span.innerText = char;
-    span.stylen.animationDelay = i*0.05 + "s";
+    span.style.animationDelay = i*0.05 + "s";
     container.appendChild(span);
-  };
+  });
+}
+
+function fadeOut(callback){
+  container.style.transition = "opacity 1s ease";
+  container.style.opacity= 0;
+
+  setTimeout(() => {
+    callback();
+    container.style.opacity = 1;
+  }, 1000);
 }
 
 function showLyrics(){
   if(index < lyrics.length){
-    waveText(lyric[index]);
+    waveText(lyrics[index]);
     index++;
     setTimeout(showLyrics, 4000);
   }
 }
 
-document.getElementById("StartBtn").onclick () => {
+document.getElementById("startBtn").onclick = () => {
   document.getElementById("overlay").style.display = "none";
   const iframe = document.getElementById("music");
   iframe.style.display = "block";
